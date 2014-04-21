fbb2fd0febc29dce078aafc42dcccde7fd96e859
Philip Ingram <philip.d.ingram@gmail.com>
2014-04-21 11:47:03 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+IceCreamFinder::Application.config.secret_key_base = '0fd8f92b3bd64c2d7da591f06aad37aa2a82f956ef7e51a1c5bf8d287aa506329580fe21ebcd00be7c1a4dca55b767941e10dd74767080b020e224595ed401f7'
